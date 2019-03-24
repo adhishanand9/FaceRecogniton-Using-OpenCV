4d7694e21cbaee02f31e62a2e9dce798c72d1e36
Adhish Anand <adhishanand9@gmail.com>
2019-03-24 17:32:16 +0530

    [Facial Recognition using OpenCV and Tkinter]

--- FaceRecognition/GUI.py
+++ FaceRecognition/GUI.py
@@ -220,13 +220,13 @@ def recognizeFace():
             cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
             id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
             # Check if confidence is less them 100 ==> "0" is perfect match 
-            if (confidence < 100):
+            if (confidence > 35):
                 attendanceID(id)
                 #id = names[id]
-                confidence = "  {0}%".format(round(100 - confidence)) 
+                confidence = "  {0}%".format(round(confidence)) 
             else:
                 id = "unknown"
-                confidence = "  {0}%".format(round(100 - confidence))
+                confidence = "  {0}%".format(round(confidence))
         
             cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
             cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
