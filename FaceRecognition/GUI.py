ea5c25f0c3f1b36ca29bc40b4e62311bcfb923bf
Adhish Anand <adhishanand9@gmail.com>
2019-03-24 17:15:43 +0530

    [Face Recognition using Tkinter and GUI]

--- FaceRecognition/GUI.py
+++ FaceRecognition/GUI.py
@@ -5,10 +5,15 @@ import numpy as np
 from PIL import Image
 import sqlite3 as lite
 import sys
+import psycopg2
+from datetime import date
+conn = psycopg2.connect(database = "testdb2", user = "postgresql", password = "namespace1", host = "sample-database.czgprnseypbr.us-east-1.rds.amazonaws.com", port = "5432")
+print ('Opened database successfully')
+cur = conn.cursor()
 
 con=lite.connect('StudentDetail.db')
 
-
+names = ['None']
 def faceDataset(ID,Name):
     cam = cv2.VideoCapture(0)
     cam.set(3, 640) # set video width
@@ -43,6 +48,8 @@ def faceDataset(ID,Name):
         cur = con.cursor()    
         cur.execute("CREATE TABLE IF NOT EXISTS student(Id INT, Name TEXT)")
         cur.execute("INSERT INTO student VALUES(?,?)",(ID,Name))
+        print(int(ID))
+        names.insert(int(ID),Name)
 
 def trainFace():
     path = 'dataset'
@@ -70,6 +77,117 @@ def trainFace():
     # Print the numer of faces trained and end program
     print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
 
+attendance=set()
+def attendanceID(roll):
+    attendance.add(roll)
+
+def upload(rollno):
+    today = str(date.today())
+    # print(today)
+
+
+    students = 40
+
+    #today = "2019-03-31"
+    # rollno = 3  
+
+    str1 = "SELECT column_name FROM information_schema.columns where table_name='mainatt' and column_name='"
+    str1 += today
+    str1 +="';"
+    cur.execute(str1)
+    rows = cur.fetchall()
+    print(rows)
+    if(rows):
+        str3 = "SELECT \""
+        str3 += today
+        str3 += "\" FROM MAINATT WHERE ROLL="
+        str3 += str(rollno)
+        str3 += ";"
+        # print(str3)
+        cur.execute(str3)
+        returned = cur.fetchone()
+        # print(returned[0])
+        if(returned[0]==1):
+            print("Already Att is marked as present , no need to do anything.")
+        else:   
+            print("Initially Absent: we need to update both the columns.")
+            str3 = "UPDATE MAINATT SET \""
+            str3 += str(today)
+            str3 += "\" =1 WHERE ROLL = "
+            str3+= str(rollno)
+            str3 += ";"
+            cur.execute(str3)
+            print("Attendence marked in main attendace.")
+            str3 = "SELECT * FROM SECOND WHERE ROLL = "
+            str3 += str(rollno)
+            str3+=";"
+            cur.execute(str3)
+            returned = cur.fetchone()
+            attended = returned[1]
+            attended = attended+1
+            str3 = "UPDATE SECOND SET ATTENDED = "
+            str3 += str(attended)
+            str3 += " WHERE ROLL = "
+            str3 += str(rollno)
+            str3 += ";"
+            cur.execute(str3)
+            print("Attended also increased.")
+
+    else:
+        print("Column Dont Exist")
+        str3 = "SELECT * FROM SECOND WHERE ROLL = "
+        str3 += str(rollno)
+        str3+=";"
+        cur.execute(str3)
+        returned = cur.fetchone()
+        total = returned[2]
+        total = total+1
+        for v in range(0,students+1):
+            str4 = "UPDATE SECOND SET TOTAL ="
+            str4 += str(total)
+            str4 += "WHERE ROLL ="
+            str4 += str(v)
+            str4 +=";"
+            cur.execute(str4)
+            print("updated total column of second")
+
+        str2 = "ALTER TABLE MAIN ATTENDANCE ADD COLUMN \""
+        str2 += str(today)
+        str2 += "\" int;"
+        print(str2)
+        cur.execute(str2)
+        print("New column created")
+        for student in range(1,students+1):
+            str3 = "UPDATE MAIN ATTENDANCE SET \""
+            str3 += str(today)
+            str3 += "\" =0 WHERE ROLL = "
+            str3+= str(student)
+            str3 += ";"
+            cur.execute(str3)
+        str3 = "UPDATE MAINATT SET \""
+        str3 += str(today)
+        str3 += "\" =1 WHERE ROLL = "
+        str3+= str(rollno)
+        str3 += ";"
+        cur.execute(str3)
+        print("Attendence marked in Main Attendance.")
+        str3 = "SELECT * FROM SECOND WHERE ROLL = "
+        str3 += str(rollno)
+        str3+=";"
+        cur.execute(str3)
+        returned = cur.fetchone()
+        attended = returned[1]
+        attended = attended+1
+        str3 = "UPDATE SECOND SET ATTENDED = "
+        str3 += str(attended)
+        str3 += " WHERE ROLL ="
+        str3+= str(rollno)
+        str3 += ";"
+        cur.execute(str3)
+        print("Attended also increased.")
+    conn.commit()
+    
+
 def recognizeFace():
     recognizer = cv2.face.LBPHFaceRecognizer_create()
     recognizer.read('trainer/trainer.yml')
@@ -79,7 +197,7 @@ def recognizeFace():
     #iniciate id counter
     id = 0
     # names related to ids: example ==> Marcelo: id=1,  etc
-    names = ['None', 'Adhish', 'Paula', 'Ilza', 'Z', 'W'] 
+    #names = ['None', 'Adhish', 'Paula', 'Ilza', 'Z', 'W'] 
     # Initialize and start realtime video capture
     cam = cv2.VideoCapture(0)
     cam.set(3, 640) # set video widht
@@ -103,8 +221,9 @@ def recognizeFace():
             id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
             # Check if confidence is less them 100 ==> "0" is perfect match 
             if (confidence < 100):
-                id = names[id]
-                confidence = "  {0}%".format(round(100 - confidence))
+                attendanceID(id)
+                #id = names[id]
+                confidence = "  {0}%".format(round(100 - confidence)) 
             else:
                 id = "unknown"
                 confidence = "  {0}%".format(round(100 - confidence))
@@ -158,3 +277,8 @@ class GUI(tk.Tk):
 
 w = GUI()
 w.mainloop()
+print(attendance)
+for i in attendance:
+    upload(i)
+conn.commit()
+conn.close()
